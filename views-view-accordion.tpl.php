3abaf2cbfe9353af4a54b2cccbb7a69795f9e6ed
Manuel Garcia <manuel.drupal@gmail.com>
2010-10-08 12:22:22 +0000

    Initial commit for the 7.x branch

--- views-view-accordion.tpl.php
+++ views-view-accordion.tpl.php
@@ -8,20 +8,20 @@
  *
  *  Note that the accordion NEEDS <?php print $row ?> to be wrapped by an element, or it will hide all fields on all rows under the first field.
  *  Also, if you use field grouping and use the headers of the groups as the accordion headers, these NEED to be inside h3 tags exactly as below (though u can add classes)
- * 
+ *
  *  The current div wraping each row gets two css classes, which should be enough for most cases:
  *     "views-accordion-item"
  *      and a unique per row class like item-0
  *
  */
 ?>
-<div class="item-list views-accordion <?php print $views_accordion_id ?>">
-  <?php if (!empty($title)): ?>
-    <h3 class="<?php print $views_accordion_id ?>"><?php print $title; ?></h3>
-  <?php endif; ?>
-  <div id="<?php print $views_accordion_id ?>">
-    <?php foreach ($rows as $id => $row): ?>
-      <div class="<?php print $classes[$id] ?>"><?php print $row ?></div>
-    <?php endforeach; ?>
+<?php if (!empty($title)): ?>
+  <h3 class="<?php print $view_accordion_id; ?>"><a href="#"><?php print $title; ?></a></h3>
+<?php endif; ?>
+<?php if($use_group_header == TRUE): ?><div><?php endif; ?>
+<?php foreach ($rows as $id => $row): ?>
+  <div class="<?php print $classes_array[$id]; ?>">
+    <?php print $row; ?>
   </div>
-</div>
+<?php endforeach; ?>
+<?php if($use_group_header == TRUE): ?></div><?php endif; ?>
