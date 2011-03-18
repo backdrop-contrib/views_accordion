8b6a6f6b518ede78a7ea1ef8bbcd86b9c1aa1fca
Manuel Garcia <manuel.drupal@gmail.com>
2011-03-18 10:23:21 +0100

    Issue #1097108 by johattan: Fix line width for doxygen comments in views-view-accordion.tpl.php

--- views-view-accordion.tpl.php
+++ views-view-accordion.tpl.php
@@ -5,10 +5,15 @@
  *
  * @ingroup views_templates
  *
- *  Note that the accordion NEEDS <?php print $row ?> to be wrapped by an element, or it will hide all fields on all rows under the first field.
- *  Also, if you use field grouping and use the headers of the groups as the accordion headers, these NEED to be inside h3 tags exactly as below (though u can add classes)
+ * Note that the accordion NEEDS <?php print $row ?> to be wrapped by an
+ * element, or it will hide all fields on all rows under the first field.
  *
- *  The current div wraping each row gets two css classes, which should be enough for most cases:
+ * Also, if you use field grouping and use the headers of the groups as the
+ * accordion headers, these NEED to be inside h3 tags exactly as below 
+ * (though you can add classes).
+ *
+ * The current div wraping each row gets two css classes, which should be
+ * enough for most cases:
  *     "views-accordion-item"
  *      and a unique per row class like item-0
  *
