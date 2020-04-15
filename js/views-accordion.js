94a4269e5c03f11443f95627523309c57e788865
Peter Anderson <BWPanda@users.noreply.github.com>
2020-04-15 23:04:21 +0300

    Enhanced navigation anchors. (#3)
    * Enhanced navigation anchors. PR submitted by @BWPanda

--- js/views-accordion.js
+++ js/views-accordion.js
@@ -2,77 +2,78 @@
  * @file
  * Javascript for views-accordion.
  */
-Backdrop.behaviors.views_accordion = {
-  attach: function(context) {
-    if(Backdrop.settings.views_accordion){
-      (function ($) {
-        $.each(Backdrop.settings.views_accordion, function(id) {
-          /* Our view settings */
-          var usegroupheader = this.usegroupheader;
-          var viewname = this.viewname;
-          var display = this.display;
-
-          /* Our panel heightStyle setting */
-          var heightStyle = (this.autoheight == 1) ? 'auto' : (this.fillspace == 1 ? 'fill' : 'content');
-
-          /* the selectors we have to play with */
-          var displaySelector = '.view-id-' + viewname + '.view-display-id-' + display + ' > .view-content';
-          console.log(displaySelector);
-          var headerSelector = this.header;
-
-          /* The row count to be used if Row to display opened on start is set to random */
-          var row_count = 0;
-
-          /* Prepare our markup for jquery ui accordion */
-          $(displaySelector + ' ' + headerSelector + ':not(.ui-accordion-header)').each(function(i){
-            // Hash to use for accordion navigation option.
-            var hash = "#" + viewname + "-" + display + "-" + i;
-            var $this = $(this);
-            var $link = $this.find('a');
-            // if the header is not already using an anchor tag, add one
-            if($link.length == 0){
-              // setup anchor tag for navigation
-              $this.wrapInner('<a href="' + hash + '"></a>');
+(function ($) {
+  Backdrop.behaviors.views_accordion = {
+    attach: function(context, settings) {
+      if(Backdrop.settings.views_accordion){
+          $.each(Backdrop.settings.views_accordion, function(id) {
+            /* Our view settings */
+            var usegroupheader = this.usegroupheader;
+            var viewname = this.viewname;
+            var display = this.display;
+            var rowids = this.rowids;
+  
+            /* Our panel heightStyle setting */
+            var heightStyle = (this.autoheight == 1) ? 'auto' : (this.fillspace == 1 ? 'fill' : 'content');
+  
+            /* the selectors we have to play with */
+            var displaySelector = '.view-id-' + viewname + '.view-display-id-' + display + ' > .view-content';
+            var headerSelector = this.header;
+  
+            /* The row count to be used if Row to display opened on start is set to random */
+            var row_count = 0;
+  
+            /* Prepare our markup for jquery ui accordion */
+            $(displaySelector + ' ' + headerSelector + ':not(.ui-accordion-header)').each(function(i){
+              // Hash to use for accordion navigation option.
+              var hash = "#" + viewname + "-" + display + "-" + rowids[i];
+              var $this = $(this);
+              var $link = $this.find('a');
+              // if the header is not already using an anchor tag, add one
+              if($link.length == 0){
+                // setup anchor tag for navigation
+                $this.wrapInner('<a href="' + hash + '"></a>');
+              }
+              // if there are already, they wont be clickable with js enabled, we'll use them for accordion navigation
+              else{
+                // @FIXME ?
+                // We are currently destroying the original link, though search crawlers will stil see it.
+                // Links in accordions are NOT clickable and leaving them would kill deep linking.
+                $link.get(0).href = hash;
+              }
+  
+              // Wrap the accordion content within a div if necessary
+              if (!usegroupheader) {
+                $this.siblings().wrapAll('<div></div>');
+              }
+              row_count++;
+            });
+  
+            if (this.rowstartopen == 'random') {
+              this.rowstartopen = Math.floor(Math.random() * row_count);
             }
-            // if there are already, they wont be clickable with js enabled, we'll use them for accordion navigation
-            else{
-              // @FIXME ?
-              // We are currently destroying the original link, though search crawlers will stil see it.
-              // Links in accordions are NOT clickable and leaving them would kill deep linking.
-              $link.get(0).href = hash;
+  
+            var options = {
+              header: headerSelector,
+              active: this.rowstartopen,
+              collapsible: this.collapsible,
+              event: this.event,
+              heightStyle: this.autoheight ? 'auto' : this.fillspace ? 'fill' : 'content',
+            };
+            if (this.animated === false) {
+              options.animate = false;
             }
-
-            // Wrap the accordion content within a div if necessary
-            if (!usegroupheader) {
-              $this.siblings().wrapAll('<div></div>');
+            else {
+              options.animate = {
+                easing: this.animated,
+                duration: this.duration,
+              }
             }
-            row_count++;
+            /* jQuery UI accordion call */
+            $(displaySelector + ':not(.ui-accordion)').accordion(options);
+            $('.views-view-accordion .views-accordion-header').has('a[href$="' + location.hash + '"]').click();
           });
-
-          if (this.rowstartopen == 'random') {
-            this.rowstartopen = Math.floor(Math.random() * row_count);
-          }
-
-          var options = {
-            header: headerSelector,
-            active: this.rowstartopen,
-            collapsible: this.collapsible,
-            event: this.event,
-            heightStyle: this.autoheight ? 'auto' : this.fillspace ? 'fill' : 'content',
-          };
-          if (this.animated === false) {
-            options.animate = false;
-          }
-          else {
-            options.animate = {
-              easing: this.animated,
-              duration: this.duration,
-            }
-          }
-          /* jQuery UI accordion call */
-          $(displaySelector + ':not(.ui-accordion)').accordion(options);
-        });
-      })(jQuery);
+      }
     }
-  }
-};
+  };
+})(jQuery);
